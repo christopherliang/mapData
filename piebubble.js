509b3c2d151107fcb0e79d632087ed6a06bcd1b5
Javis <wujavis@gmail.com>
2016-03-30 03:13:19 -0400

    should be gooood

--- piebubble.js
+++ piebubble.js
@@ -75,7 +75,79 @@ function type(d) {
     //console.log(d)
   return d;
 }
+}//end of if
+else{ //REPUBLICAN
+    d3.select("#error").html("");
+    
+    
+    var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+    
+var color = d3.scale.ordinal()
+	.range(["red", "orange","yellow","purple"]);
+    
+    var arc = d3.svg.arc()
+	.outerRadius(radius - 10)
+	.innerRadius(0);
+    
+    var labelArc = d3.svg.arc()
+	.outerRadius(radius - 40)
+	.innerRadius(radius - 40);
 
+    var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) { 
+	    
+	    console.log(d[userState]);
+	
+	    return d[userState]; });
+    
+    var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    
+    d3.csv("https://raw.githubusercontent.com/christopherliang/mapData/master/newRepub.csv", type, function(error, data) {
+	if (error) throw error;
+	
+	var g = svg.selectAll(".arc")
+	    .data(pie(data))
+    .enter().append("g")
+	    .attr("class", "arc");
+	
+	g.append("path")
+	    .transition()
+	    .duration(1000)
+	    .attr("d", arc)
+	    .style("fill", function(d) { 
+		return color(d.data.Candidate); });
+  g.append("text")
+	    .transition()
+	    .duration(1000)
+	    .attr("transform", function(d) { 
+		//console.log(d);
+		//console.log(labelArc.centroid(d));
+		
+	  return "translate(" + labelArc.centroid(d) + ")"; })
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.data.Candidate; });
+    });
+    var holder;
+    function type(d) {
+	//console.log(d);
+	//console.log("then");
+	var headerNames=d3.keys(d);
+	holder=headerNames;
+	console.log(d3.keys(d));
+	for (x=1;x<headerNames.length;x++){
+	  d[headerNames[x]] = +d[headerNames[x]];
+	}
+	//console.log(d)
+	return d;
+    }
+}
 
 d3.select('#state')
   .on('change',function(){
@@ -108,7 +180,7 @@ d3.select('#state')
 	      return d[userState]; });*/
   });
 console.log(userState);
-}
+
 }
 repeat();
 button=document.getElementById("changeParty");
@@ -116,9 +188,12 @@ button.addEventListener("click",function(){
     console.log("ere");
     if(isDemo){
 	isDemo=false;
+	d3.select('#party').html('Republican Delegate Breakdown');
     }
     else{
 	isDemo=true;
+	d3.select('#party').html('Democratic Delegate Breakdown');
+
     }
 })
 console.log(isDemo);
